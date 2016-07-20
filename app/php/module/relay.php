bd11b9fa13601256e0c1edbf94e9a63aa9f7aadc
root <user.loca@gmail.com>
2016-07-20 17:50:08 +1000

    [consolidate] app/php/module/* -> relay.php

---
+++ app/php/module/relay.php
@@ -0,0 +1,79 @@
+<?php namespace relay\module; ?>
+<?php class relay {
+
+
+
+
+    /** var - invalid(s) **/
+    private $invalid_relays = array(
+        '.',
+        '..'
+    );
+
+
+
+
+    /** create **/
+    public function create() {
+
+        // check(s)
+        if( ! isset($_POST['name'])       ){ throw new \exception("Missing required argument 'name'");  }
+        if( ! ctype_alnum($_POST['name']) ){ throw new \exception("Invalid argument 'name'");           }
+
+        // relay
+        mkdir(DIR_RELAY."/".$_POST['name']);
+
+        // ? - error
+        if( ! is_dir(DIR_RELAY."/".$_POST['name']) ){
+            throw new \exception("Unable to relay relay");
+        }
+
+        // return
+        return true;
+
+    }
+
+
+
+
+    /** delete **/
+    public function delete() {
+
+        // check(s)
+        if( ! isset($_POST['name'])       ){ throw new \exception("Missing required argument 'name'");  }
+        if( ! ctype_alnum($_POST['name']) ){ throw new \exception("Invalid argument 'name'");           }
+
+        // ? - already - deleted
+        if( ! is_dir(DIR_RELAY."/".$_POST['name']) ){
+            return true;
+        }
+
+        // delete
+        rmdir(DIR_RELAY."/".$_POST['name']);
+
+        // ? - error - delete
+        if(is_dir(DIR_RELAY."/".$_POST['name']) ){
+            throw new \exception("Unable to delete relay");
+        }
+
+        // return
+        return true;
+
+    }
+
+
+
+
+    /** get **/
+    public function get() {
+                $relays = scandir(DIR_RELAY);
+                $relays = array_diff($relays, $this->invalid_relays);
+                $relays = array_values($relays);
+        return  $relays;
+    }
+
+
+
+
+} ?>
+<?php $object = new index(); ?>
