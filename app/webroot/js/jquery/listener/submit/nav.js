13dc104ce0dec0979a4d618c8eb3debd1175b995
root <user.loca@gmail.com>
2016-08-08 01:21:00 +1000

    [update] app/webroot/jquery

--- app/webroot/js/jquery/listener/submit/nav.js
+++ app/webroot/js/jquery/listener/submit/nav.js
@@ -1,7 +1,19 @@
-$("#input-relay").submit(function() {
+$("#form-relay").submit(function() {
 
 
+    // get - relay - name
+    var relay_name = $("#input-relay").val();
 
+    // api - relay - files
+    api = relay.get_files(relay_name);
+    api.success(function() {
+        console.log('- success');
+        console.log(data);
+    });
+    api.error(function() {
+        console.log('- error');
+        console.log(data);
+    });
 
 
 });
