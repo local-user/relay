e64b0398ae524067d0c9251bfe1da997f698f531
root <user.loca@gmail.com>
2016-08-11 00:32:53 +1000

    [update] php/relay.php

--- app/php/object/relay.php
+++ app/php/object/relay.php
@@ -1,12 +1,14 @@
 <?php namespace relay\module; ?>
-<?php class relay {
+<?php class     relay {
 
 
 
 
-    /** global(s) **/
-    private $relay          = null;
-    private $invalid_relays = array(
+    /** var - global(s)) **/
+    private $relay = null;
+
+    /** var - invalid(s) **/
+    private $invalid_files = array(
         '.',
         '..'
     );
@@ -14,87 +16,45 @@
 
 
 
-    /** | create **/
+    /** | __ **/
 
-        public function create() {
-
-            // check(s)
-            if( ! isset($_POST['name'])       ){ throw new \exception("Missing required argument 'name'");  }
-            if( ! ctype_alnum($_POST['name']) ){ throw new \exception("Invalid argument 'name'");           }
-
-            // relay
-            mkdir(DIR_RELAY."/".$_POST['name']);
-
-            // ? - error
-            if( ! is_dir(DIR_RELAY."/".$_POST['name']) ){
-                throw new \exception("Unable to relay relay");
-            }
-
-            // return
-            return true;
-
+        public function __construct() {
+            if( isset($_GET['relay']) ){ $this->relay = $relay; }
         }
 
-    /** create | **/
-
-
-    /** | delete **/
-
-        public function delete() {
-
-            // set - relay
-            $this->set_relay();
-
-            // ? - already - deleted
-            if( ! is_dir(DIR_RELAY."/".$this->relay) ){ return true;}
-
-            // delete
-            rmdir(DIR_RELAY."/".$this->relay);
-
-            // ? - error - delete
-            if( is_dir(DIR_RELAY."/".$relay){
-                throw new \exception("Unable to delete relay");
-            }
-
-            // return
-            return true;
-
-        }
-
-    /** delete | **/
+    /** **| **/
 
 
 
     /** | list **/
 
         public function list() {
-                    $relays = scandir(DIR_RELAY);
-                    $relays = array_diff($relays, $this->invalid_relays);
-                    $relays = array_values($relays);
-            return  $relays;
+
+                        // var - list
+                         $list  = array();
+
+                        // var - relay
+                        $relay = DIR_RELAYS.'/'.$this->relay;
+
+                        // prepare - relay - files
+                        $files = scandir($relay);
+                        $files = array_diff($files, $this->invalid_files);
+                        $files = array_values($files);
+
+                // prepare- file(s) - extra
+                foreach($files as $file){
+                    $list[] = array(
+                        'filename'      => $file,
+                        'date_modified' => date ("H:i:s - d-m-Y", filemtime($relay.'/'.$file))
+                    );
+                }
+
+            // return
+            return  $list;
+
         }
 
     /** list | **/
-
-
-
-
-    /** | set **/
-
-        public function set_relay($relay = null) {
-            if( ! $relay ){
-                if( !       isset($_POST['relay']) ){ throw new \exception("Missing required argument 'name'");  }
-                if( ! ctype_alnum($_POST['relay']) ){ throw new \exception("Invalid argument 'name'");           }
-                         $relay = $_POST['relay'];
-            }
-            if( ! is_dir(DIR_RELAY.'/'.$relay) ){
-                return false;
-            }
-            $this->relay = $relay;
-            return true;
-        }
-
-    /** set | **/
 
 
 
