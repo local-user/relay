e75714ad231cfed820a861458c55ff0470fda147
root <user.loca@gmail.com>
2016-08-11 11:40:34 +1000

    [cleanup] php/api.php

--- app/php/api.php
+++ app/php/api.php
@@ -36,122 +36,152 @@
 
 
 
-    /** __ - construct **/
-    public function __construct() {
-        $this->detect_object();
-        $this->detect_method();
-        $this->exec();
-    }
+    /** | __ **/
 
-    /** __ - destruct **/
-    public function __destruct() {
-        $this->display_json();
-    }
-
-
-
-
-    /** detect - object **/
-    private function detect_object($object = null) {
-        if(isset($_GET['object']))  { 
-            $object = $_GET['object'];
-                unset($_GET['object']);
+        public function __construct() {
+            if( $this->check_args() ){
+                $this->detect_object();
+                $this->detect_method();
+                $this->exec();
+                return true;
+            } else {
+                $this->response_code = 400;
+                $this->response_data = array(
+                    'message' => 'bad arguments'
+                );
+                return false;
+            }
         }
-        if(isset($_POST['object'])) {
-            $object = $_POST['object'];
-                unset($_POST['object']);
-        }
-        if(in_array($object, $this->valid_objects)) {
-            $this->request_object = $object;
+
+        /** __ - destruct **/
+        public function __destruct() {
+            $this->display_json();
             return true;
-        } else {
-            return false;
         }
-    }
+
+    /** __ | **/
 
 
-    /** detect - method **/
-    private function detect_method($method = null) {
-        if(isset($_GET['method']))  {
-            $method = $_GET['method'];
-                unset($_GET['method']);
-        }
-        if(isset($_POST['method'])) {
-            $method = $_POST['method'];
-                unset($_POST['method']);
-        }
-        if($method) {
-            $this->request_method = $method;
+
+
+    /** | check **/
+
+        private function check_args() {
+            foreach( $_GET as $key => $value ){
+                if( ! ctype_alnum($value) ){
+                    return false;
+                }
+            }
             return true;
-        } else {
-            return false;
         }
-    }
+
+    /** check | **/
 
 
 
 
-    /** display - json **/
-    private function display_json() {
-        header('Content-Type: application/json');
-        http_response_code($this->response_code);
-        echo json_encode($this->response_data);
-        return true;
-    }
+    /** | detect **/
+
+        private function detect_object($object = null) {
+            if(isset($_GET['object']))  { 
+                $object = $_GET['object'];
+                    unset($_GET['object']);
+            }
+            if(in_array($object, $this->valid_objects)) {
+                $this->request_object = $object;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        private function detect_method($method = null) {
+            if(isset($_GET['method']))  {
+                $method = $_GET['method'];
+                    unset($_GET['method']);
+            }
+            if($method) {
+                $this->request_method = $method;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    /** detect | **/
 
 
 
 
-    /** exec **/
-    private function exec() {
-        try {
-            $this->init_dir();
-            $this->response_code = 200;
-            $this->response_data = array(
-                $this->exec_request()
-            );
+    /** | display **/
+
+        private function display_json() {
+            header('Content-Type: application/json');
+            http_response_code($this->response_code);
+            echo json_encode($this->response_data);
             return true;
-        } catch(\exception $e) {
-            if(FLAG_DEBUG){ print_r($e); }
-            $this->response_code = 500;
-            $this->response_data = array(
-                'message' => $e->getMessage()
-            );
-            return false;
         }
-    }
 
-    /** exec - request **/
-    private function exec_request() {
-        $object = $this->request_object;
-        $method = $this->request_method;
-        if( ! $object ){
-            throw new \exception('Invalid object');
-        }
-        if( ! $method ){
-            throw new \exception('Invalid method');
-        }
-        if(  file_exists(__DIR__.'/object/'.$object.'.php')) {
-            require_once(__DIR__.'/object/'.$object.'.php');
-        } else {
-            throw new \exception('object not found');
-        }
-        if( ! method_exists($object, $method)) {
-            throw new \exception('Method not found');
-        }
-        return $object->$method();
-    }
+    /** display | **/
 
 
 
 
-    /** init - dir **/
-    private function init_dir() {
-        if( ! is_dir(DIR_RELAYS)      ){ mkdir(DIR_RELAYS);                                                                  }
-        if( ! is_dir(DIR_RELAYS)      ){ throw new \exception("System relay directory '".DIR_RELAYS."' could not be found"); }
-        if( ! is_writable(DIR_RELAYS) ){ throw new \exception("System relay directory '".DIR_RELAYS."' is not writeable");   }
-        return true;
-    }
+    /** | exec **/
+
+        private function exec() {
+            try {
+                $this->init_dir();
+                $this->response_code = 200;
+                $this->response_data = array(
+                    $this->exec_request()
+                );
+                return true;
+            } catch(\exception $e) {
+                if(FLAG_DEBUG){ print_r($e); }
+                $this->response_code = 500;
+                $this->response_data = array(
+                    'message' => $e->getMessage()
+                );
+                return false;
+            }
+        }
+
+        private function exec_request() {
+            $object = $this->request_object;
+            $method = $this->request_method;
+            if( ! $object ){
+                throw new \exception('Invalid object');
+            }
+            if( ! $method ){
+                throw new \exception('Invalid method');
+            }
+            if(  file_exists(__DIR__.'/object/'.$object.'.php')) {
+                require_once(__DIR__.'/object/'.$object.'.php');
+            } else {
+                throw new \exception('object not found');
+            }
+            if( ! method_exists($object, $method)) {
+                throw new \exception('Method not found');
+            }
+            return $object->$method();
+        }
+
+    /** exec | **/
+
+
+
+
+    /** | init **/
+
+        private function init_dir() {
+            if( ! is_dir(DIR_RELAYS)      ){ mkdir(DIR_RELAYS);                                                                  }
+            if( ! is_dir(DIR_RELAYS)      ){ throw new \exception("System relay directory '".DIR_RELAYS."' could not be found"); }
+            if( ! is_writable(DIR_RELAYS) ){ throw new \exception("System relay directory '".DIR_RELAYS."' is not writeable");   }
+            return true;
+        }
+
+    /** init | **/
 
 
 
